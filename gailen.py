e7def333515e8323e39776a8041e223fd61080bc
iamgillespie <gailen.gillespie@gmail.com>
2022-05-14 08:08:08 -0500

    pyscript testing

--- gailen.py
+++ gailen.py
@@ -1,3 +1,25 @@
+from js import console
 
+### REFERENCE VIDEO https://www.youtube.com/watch?v=7meW2djIUYk&t=4s
 
-#pyscript.write('message', 'testing')
+### TODO - BUILD CONTACT FORM AND BLOG USING PYSCRIPT
+
+#introduce tags to python
+    #<input type="text" id="pyinput" class="form-control" placeholder="Enter some text">
+pyinput = Element('pyinput')
+    #<button type="button" class="btn btn-light" id="submit">Submit</button>
+button = Element('submit')
+    #<div class="pydiv">
+div = Element('pydiv')
+
+def showthethings(*args):
+    #console.log('button was clicked')
+    output = pyinput.element.value
+    newthing = create("p", classes='output text-center')
+    newthing.element.innerText = output
+    div.element.appendChild(newthing.element)
+
+    pyinput.clear()
+   
+
+button.element.onclick = showthethings
