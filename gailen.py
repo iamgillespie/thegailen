60355a5416cf913be0186e83cc80a873c76f3ec3
iamgillespie <gailen.gillespie@gmail.com>
2022-05-14 16:50:07 -0500

    menu and contact form changes

--- gailen.py
+++ gailen.py
@@ -8,6 +8,7 @@ from js import console
     #variable = Element('tag-id')
 name = Element('name')
 email = Element('email')
+phone = Element('phone')
 message = Element('message')
 button = Element('submit')
     #<div class="pydiv">
@@ -17,10 +18,19 @@ def showthethings(*args):
     #console.log('button was clicked')
     nameout = name.element.value
     emailout = email.element.value
+    phoneout = phone.element.value
     messageout = message.element.value
-    newthing = create("p", classes='output text-center')
-    newthing.element.innerText = nameout + ' ' + emailout + ' ' + messageout
-    div.element.appendChild(newthing.element)
+    confirmation = create("div", classes='output text-center alert alert-info fade show')
+    confirmation.element.innerText = 'Your message has been sent.'
+    div.element.appendChild(confirmation.element)
+
+    userinfo = create("div", classes='output text-center alert alert-primary fade show')
+    userinfo.element.innerText = nameout + '\n' + emailout + '\n' + phoneout
+    div.element.appendChild(userinfo.element)
+
+    messageinfo = create("div", classes='output alert alert-primary fade show')
+    messageinfo.element.innerText = 'Message: ' '\n' + messageout
+    div.element.appendChild(messageinfo.element)
 
     name.clear()
    
